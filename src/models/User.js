41909224d0de345338223b40b48f81395d5c9827
karladelacruz <karlacicler@hotmail.com>
2023-05-16 10:15:47 -0600

    first commit

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+UserSchema.methods.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+UserSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default mongoose.model("User", UserSchema);
